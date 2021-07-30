f6f5cd8e61d375668a46440aa1985b44891b62f4
angueu <1345298538@qq.com>
2021-07-30 12:54:32 +0800

    4

---
+++ ciyunyan4/sadudysajbhn.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+//int main() {
+//	int s = 0;
+//	int a = 3, b = 5, c = 6;
+//	int i = ++a && b++ && ++c;
+//	printf("%d\n", i);
+//	printf("a=%d\nb=%d\nc=%d\n", a, b, c);
+//	return 0;
+//}
+//int main() {
+//	int a[10] = { 0,1,2,3 };
+//	printf("%d\n", a[0]);
+//	return 0;
+//}
