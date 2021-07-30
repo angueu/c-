fc2e13ecda2c7086d2127e82d4d05057a34986c2
angueu <1345298538@qq.com>
2021-07-30 19:04:21 +0800

    5

---
+++ cyuyan-study5/sajkdsad.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+////int main() {
+//	int b = 12;
+//	int* pa = &b;
+//	printf("%p\n", pa + 1);
+//	return 0;
+//}
+//int main() {
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	int* pc = &arr;
+//	for (i = 0; i <= sizeof(arr) / sizeof(arr[0]); i++) 
+//	{
+//		*(pc + i) = 1;
+//		printf("%d\n", arr[i]);
+//	}
+//	
+//	return 0;
+//}
