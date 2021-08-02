f484745f90cfbce9259af9660e48073ab9ffee20
angueu <1345298538@qq.com>
2021-08-02 17:32:41 +0800

    练习

---
+++ cyuyan6/akjdksjdkas.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main() {
+	/*int arr[] = { 1,2,9 };
+	int a = &arr[2] - &arr[1];
+	printf("%d\n", a);
+	return 0;*/
+	int i;
+	for (i = 1; i <= 10;) {
+		i++;
+	}
+	printf("%d", i);
+	return 0;
+}
