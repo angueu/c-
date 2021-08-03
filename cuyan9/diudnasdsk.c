d7797fb1c414e63a907bfa2dfb1c2bf9c7bd3cc6
angueu <1345298538@qq.com>
2021-08-03 15:54:56 +0800

    有个小错误

---
+++ cuyan9/diudnasdsk.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+//int main() {
+//	int a = 0x11223344;
+//	int b = 010;
+//	printf("%d %d", a, b);
+//	return 0;
+//
+//
+//}
+//int sz(int n){
+//	int count = 0;
+//	for (n=n; n % 2 == 0; n = n / 2)
+//	{
+//		if (n % 2 == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
+//
+//int main() {
+//	int tem = 0;
+//	
+//	scanf_s("%d",&tem);
+//	int count = sz(tem);
+//	printf("%d ", count);
+//	return 0;
+//}
+//int main() {
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1; i <= 12;i=i*2) 
+//	{
+//		count++;
+//	}
+//	printf("%d ", count);
+//	return 0;
+//}
